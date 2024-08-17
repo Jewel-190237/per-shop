87d2d2e15854f026314c2fab06be8bc629ca48a1
Jewel-190237 <jewel190237@gmail.com>
2024-08-17 12:39:54 +0600

    payment finished

--- src/Components/Payment/Payment3.jsx
+++ src/Components/Payment/Payment3.jsx
@@ -4,13 +4,16 @@ import pussyBanner from '../../assets/image/payment/pussyCuteBanner.png'
 import chatIcon from '../../assets/image/payment/chatIcon.png'
 import person from '../../assets/image/payment/chatPerson.png'
 import callIcon from '../../assets/image/payment/Call btn.png'
+import file from '../../assets/image/payment/file.png'
+import camera from '../../assets/image/payment/camera.png'
+import aero from '../../assets/image/payment/Arrow - Right 2.png'
 
 const Payment3 = () => {
     return (
         <section>
             <div className='bg-[#61D5DF] h-60 w-full mb-20 mt-56 flex justify-between relative'>
-                <div className='ml-4 md:ml-20 flex flex-col items-center justify-center'>
-                    <h1 className='text-[#181818] font-bold text-3xl pl-5'>Chat</h1>
+                <div className='ml-4 md:ml-20 flex flex-col justify-center'>
+                    <h1 className='text-[#181818] font-bold text-3xl '>Chat</h1>
                     <div className='flex space-x-2'>
                         <img src={homeIcon} alt="" />
                         <p className='mt-3'>Home /<span className='text-[#FF5C2C]'> Shop</span></p>
@@ -22,14 +25,14 @@ const Payment3 = () => {
             </div>
 
             <div className='max-w-[1320px]'>
-                <div className='grid grid-cols-1 md:grid-cols-3 bg-[#FFFFFF] shadow shadow-[#0000004D] rounded-xl p-4 mt-32 gap-16'>
+                <div className='grid grid-cols-1 md:grid-cols-3 bg-[#FFFFFF] shadow shadow-[#0000004D] rounded-xl p-4 mt-32 gap-16 mb-10'>
                     <div className=''>
-                        <div className='flex gap-4 items-center'>
+                        <div className='flex gap-4 items-center mt-4'>
                             <img src={chatIcon} alt="" />
                             <h4 className='font-bold text-xl'>All Conversion</h4>
                         </div>
                         <div className='flex justify-between mt-16 items-center'>
-                            <div className='flex items-center gap-6'>
+                            <div className='flex items-center gap-6 p-3'>
                                 <img src={person} alt="" />
                                 <div>
                                     <p>Shopons World</p>
@@ -54,7 +57,7 @@ const Payment3 = () => {
                         </div>
                     </div>
                     <div className='col-span-2'>
-                        <div className='flex justify-between items-center'>
+                        <div className='flex justify-between items-center mb-10'>
                             <div className='flex items-center gap-6'>
                                 <img src={person} alt="" />
                                 <div>
@@ -65,6 +68,45 @@ const Payment3 = () => {
                             <div>
                                 <img src={callIcon} alt="" />
                             </div>
+                        </div>
+                        <div>
+                            <div className="chat chat-start">
+                                <div className="chat-image avatar">
+                                    <div className="w-10 rounded-full">
+                                        <img src={person} alt="" />
+                                    </div>
+                                </div>
+                                <div className="chat-header">
+                                    Obi-Wan Kenobi
+                                    <time className="text-xs opacity-50">12:45</time>
+                                </div>
+                                <div className="chat-bubble">You were the Chosen One!</div>
+                                <div className="chat-footer opacity-50">Delivered</div>
+                            </div>
+                            <div className="chat chat-end pr-8">
+                                <div className="chat-image avatar">
+                                    <div className="w-10 rounded-full">
+                                        <img src={person} alt="" />
+                                    </div>
+                                </div>
+                                <div className="chat-header">
+                                    Anakin
+                                    <time className="text-xs opacity-50">12:46</time>
+                                </div>
+                                <div className="chat-bubble bg-[#FF5C2C]">I hate you!</div>
+                                <div className="chat-footer opacity-50">Seen at 12:46</div>
+                            </div>
+                        </div>
+                        <div className='w-full px-8'>
+                            {/* <input className='w-full input input-bordered border-[#FF5C2C]' type="text" placeholder='Write Your message'/> */}
+                            <label className="input input-bordered border-[#FF5C2C] flex items-center justify-between ">
+                                <input type="text" className="grow" placeholder="Write Your message" />
+                                <div className='flex items-center gap-4'>
+                                    <img className='w-6 h-6' src={file} alt="" />
+                                    <img className='w-6 h-6' src={camera} alt="" />
+                                    <img className='w-8 h-8 bg-[#FF5C2C] rounded-lg p-2 text-3xl' src={aero} alt="" />
+                                </div>
+                            </label>
                         </div>
                     </div>
                 </div>
