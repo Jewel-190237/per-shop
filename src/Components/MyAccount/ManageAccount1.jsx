b7d3d26909e5441eb1c801bf99db6cc60b9e962f
Jewel-190237 <jewel190237@gmail.com>
2024-08-18 23:16:49 +0600

    file updated10

--- src/Components/MyAccount/ManageAccount1.jsx
+++ src/Components/MyAccount/ManageAccount1.jsx
@@ -17,8 +17,60 @@ const ManageAccount1 = () => {
                     <img src={pussyBanner} alt="" className='w-64 md:w-full' />
                 </div>
             </div>
-            <div>
-                
+            <div className='max-w-[1320] mx-auto'>
+                <div className='grid grid-cols-2 md:grid-cols-4 p-3 mt-32 gap-8'>
+                    <div>
+                        <h4 className='text-xl font-bold'>Hello, Leton Nondi</h4>
+                        <h4 className='mt-8 text-xl font-bold'>Manage My Account</h4>
+                        <div className='mt-8'>
+                            <a href=""><p className='mt-2 text-[#9da6b1] font-bold'>My Profile</p></a>
+                            <a href=""><p className='mt-2 text-[#9da6b1] font-bold'>Address Book</p></a>
+                            <a href=""><p className='mt-2 text-[#9da6b1] font-bold'>MY Payment Options </p></a>
+                            <a href=""><p className='mt-2 text-[#9da6b1] font-bold'>Vouchers</p></a>
+                        </div>
+                        <h4 className='mt-8 text-xl font-bold'>My Orders</h4>
+                        <div className='mt-8'>
+                            <a href=""><p className='mt-2 text-[#9da6b1] font-bold'>My Returns</p></a>
+                            <a href=""><p className='mt-2 text-[#9da6b1] font-bold'>My Cancelation</p></a>
+                        </div>
+                        <h4 className='mt-8 text-xl font-bold'>My Review</h4>
+                        <h4 className='mt-8 text-xl font-bold'>Sell with us</h4>
+                    </div>
+                    <div>
+                        <h4 className='text-xl font-bold'>Manage my Account</h4>
+                        <div className='bg-[#FF5C2C1A] rounded-lg p-4'>
+                            <h4 className='font-bold'>Personal Profile | EDIT</h4>
+                            <div className='mt-4'>
+                                <a href=""><p className='mt-2 text-[#9da6b1] font-bold'>Leton Nondi</p></a>
+                                <a href=""><p className='mt-2 text-[#9da6b1] font-bold'>letonnondi@gmail.com</p></a>
+                            </div>
+                            <h4 className='mt-20 font-bold text-[15px] pb-8 text-[#FF5C2C]'>Subscribe Our Newsletter</h4>
+                        </div>
+                    </div>
+                    <div className='col-span-2 mt-7'>
+                        <div className='flex justify-between gap-4 bg-[#FF5C2C1A] rounded-lg'>
+                            <div className='flex-1 rounded-lg p-4'>
+                                <h4 className='font-bold'>Address Book | <span className='text-[#FF5C2C]'>EDIT</span></h4>
+                                <p className='mt-2 text-[#9da6b1]  uppercase'>DEFOULT SHIPPING ADDRESS</p>
+                                <div className='mt-4'>
+                                    <a href=""><p className='mt-2 text-[#9da6b1] font-bold'>Leton Nondi</p></a>
+                                    <a href=""><p className='mt-2 text-[#9da6b1] font-bold'>letonnondi@gmail.com</p></a>
+                                </div>
+                                <h4 className='mt-20 font-bold text-[15px] pb-8 text-[#FF5C2C]'>Subscribe Our Newsletter</h4>
+                            </div>
+                            <div className='flex-1 rounded-lg p-4'>
+                                <h4 className='font-bold'>Personal Profile | EDIT</h4>
+                                <div className='mt-4'>
+                                    <a href=""><p className='mt-2 text-[#9da6b1] font-bold'>Leton Nondi</p></a>
+                                    <a href=""><p className='mt-2 text-[#9da6b1] font-bold'>letonnondi@gmail.com</p></a>
+                                </div>
+                                <h4 className='mt-20 font-bold text-[15px] pb-8 text-[#FF5C2C]'>Subscribe Our Newsletter</h4>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </section>
     );
