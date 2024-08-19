5e3c00c378279c60e25af20392e64ffababec373
Jewel-190237 <jewel190237@gmail.com>
2024-08-19 11:43:18 +0600

    paymentOption2 added

--- src/Components/MyAccount/ManageAccount1.jsx
+++ src/Components/MyAccount/ManageAccount1.jsx
@@ -24,7 +24,7 @@ const ManageAccount1 = () => {
                         <a href=""><h4 className='mt-8 text-xl font-bold'>Manage My Account</h4></a>
                         <div className='mt-8'>
                             <a href="/myProfile"><p className='mt-2 text-[#9da6b1] font-bold'>My Profile</p></a>
-                            <a href=""><p className='mt-2 text-[#9da6b1] font-bold'>Address Book</p></a>
+                            <a href="/addressBook"><p className='mt-2 text-[#9da6b1] font-bold'>Address Book</p></a>
                             <a href=""><p className='mt-2 text-[#9da6b1] font-bold'>MY Payment Options </p></a>
                             <a href=""><p className='mt-2 text-[#9da6b1] font-bold'>Vouchers</p></a>
                         </div>
---
+++ src/Components/MyAccount/MyPaymentOption.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyPaymentOption = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default MyPaymentOption;
