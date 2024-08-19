b1ed28d15c53a8a402466406e6f6734fa2efcd8f
Jewel-190237 <jewel190237@gmail.com>
2024-08-19 14:30:10 +0600

    vouchers added

--- src/Components/MyAccount/AddressBook.jsx
+++ src/Components/MyAccount/AddressBook.jsx
@@ -1,23 +1,11 @@
 import React from 'react';
 import homeIcon from '../../assets/image/singleShop/homeIcon.png'
 import pussyBanner from '../../assets/image/singleShop/pussyCuteBanner.png'
+import Banner from '../Shared/Banner/Banner';
 const AddressBook = () => {
     return (
         <section>
-            <div className='bg-[#61D5DF] h-60 w-full mb-20 mt-56 flex justify-between relative'>
-                <div className='w-96 mx-auto'>
-                    <div className=' flex flex-col justify-center'>
-                        <h1 className='text-[#181818] font-bold text-3xl'>Our Shop</h1>
-                        <div className='flex space-x-2'>
-                            <img src={homeIcon} alt="" />
-                            <p className='mt-3'>Home /<span className='text-[#FF5C2C]'> Shop</span></p>
-                        </div>
-                    </div>
-                    <div className='absolute right-0 bottom-0'>
-                        <img src={pussyBanner} alt="" className='w-64 md:w-full' />
-                    </div>
-                </div>
-            </div>
+            <Banner></Banner>
             <div className='max-w-[1320px] mx-auto'>
                 <div className='grid grid-cols-1 md:grid-cols-4 p-3 mt-32 rounded-lg '>
                     <div>
--- src/Components/MyAccount/MyCanceletion.jsx
+++ src/Components/MyAccount/MyCanceletion.jsx
@@ -2,22 +2,12 @@ import React from 'react';
 import homeIcon from '../../assets/image/singleShop/homeIcon.png'
 import pussyBanner from '../../assets/image/singleShop/pussyCuteBanner.png'
 import item1 from '../../assets/image/myAccount/Item_01.png'
+import Banner from '../Shared/Banner/Banner';
 
 const MyCancelation = () => {
     return (
         <section>
-            <div className='bg-[#61D5DF] h-60 w-full mb-20 mt-56 flex justify-between relative'>
-                <div className='ml-4 md:ml-20 flex flex-col items-center justify-center'>
-                    <h1 className='text-[#181818] font-bold text-3xl pl-5'>Our Shop</h1>
-                    <div className='flex space-x-2'>
-                        <img src={homeIcon} alt="" />
-                        <p className='mt-3'>Home /<span className='text-[#FF5C2C]'> Shop</span></p>
-                    </div>
-                </div>
-                <div className='absolute right-0 bottom-0'>
-                    <img src={pussyBanner} alt="" className='w-64 md:w-full' />
-                </div>
-            </div>
+           <Banner></Banner>
             <div className='max-w-[1320] mx-auto'>
                 <div className='grid grid-cols-1 md:grid-cols-4 p-3 mt-32 rounded-lg '>
                     <div>
