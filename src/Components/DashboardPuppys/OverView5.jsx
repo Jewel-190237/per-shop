b7ad50ac1e719f601bebd2e73fa68c19b7cb66c4
Jewel-190237 <jewel190237@gmail.com>
2024-08-20 16:23:48 +0600

    Overview5 added

---
+++ src/Components/DashboardPuppys/OverView5.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Banner from '../Shared/Banner/Banner';
+
+const OverView5 = () => {
+    return (
+        <section>
+            <div>
+                <Banner></Banner>
+            </div>
+            <div className='max-w-[1320px] mx-auto'>
+
+            </div>
+            
+        </section>
+    );
+};
+
+export default OverView5;
