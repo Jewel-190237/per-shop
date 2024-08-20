bc611bc00fee17fc701cf8e23de7f627ea1cfd46
Jewel-190237 <jewel190237@gmail.com>
2024-08-20 17:55:31 +0600

    file uploadede

--- src/Components/PopularBread/PopularBread.jsx
+++ src/Components/PopularBread/PopularBread.jsx
@@ -1,4 +1,3 @@
-
 import React, { useRef, useState } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -8,64 +7,49 @@ import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
 
-import './style.css';
+import './styles.css';
 
 // import required modules
 import { EffectCoverflow, Pagination } from 'swiper/modules';
+
 
 import sliderImage from '../../assets/image/PopularBreads/sliderImage.png'
 
 const PopularBread = () => {
     return (
-        <section className='max-w-6xl mx-auto'>
-            <div className='text-center'>
+        <section className='max-w-[1320px] mx-auto'>
+            {/* <div className='text-center'>
                 <h3 className='text-2xl font-bold mb-2 text-black'>Featured Services</h3>
                 <p className='max-w-lg mx-auto'>Designed so animals can relax & indulge in pet stylists, run around our exercise yards, make new friends in doggy daycare, or simply spend hours in our dog.</p>
-            </div>
-            <Swiper
-                effect={'coverflow'}
-                grabCursor={true}
-                centeredSlides={true}
-                slidesPerView={'auto'}
-                coverflowEffect={{
-                    rotate: 50,
-                    stretch: 0,
-                    depth: 100,
-                    modifier: 1,
-                    slideShadows: true,
-                }}
-                pagination={true}
-                modules={[EffectCoverflow, Pagination]}
-                className="mySwiper"
-            >
-                <SwiperSlide>
-                    <img className='' src={sliderImage} />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img className='' src={sliderImage} />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img className='' src={sliderImage} />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img className='' src={sliderImage} />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img className='' src={sliderImage} />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img className='' src={sliderImage} />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img className='' src={sliderImage} />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img className='' src={sliderImage} />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img className='' src={sliderImage} />
-                </SwiperSlide>
-            </Swiper>
+            </div> */}
+            
+                <Swiper
+                    effect={'coverflow'}
+                    grabCursor={true}
+                    centeredSlides={true}
+                    slidesPerView={'auto'}
+                    coverflowEffect={{
+                        rotate: 50,
+                        stretch: 0,
+                        depth: 100,
+                        modifier: 1,
+                        slideShadows: true,
+                    }}
+                    pagination={true}
+                    modules={[EffectCoverflow, Pagination]}
+                    className="mySwiper"
+                >
+                    <SwiperSlide>
+                        <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+                    </SwiperSlide>
+                </Swiper>
+            
         </section>
     );
 };
