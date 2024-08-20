a94e61c1542db22f528846ae550389bcf070e5d1
Jewel-190237 <jewel190237@gmail.com>
2024-08-20 10:30:40 +0600

    file added

---
+++ src/Components/DashboardPuppys/AddPyppy.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Banner from '../Shared/Banner/Banner';
+
+const AddPuppy = () => {
+    return (
+        <section>
+            <div>
+                <Banner></Banner>
+            </div>
+            <div className='max-w-[1320px] bg-[#FF5C2C1A] rounded-lg'>
+                <div>
+
+                </div>
+
+            </div>
+        </section>
+    );
+};
+
+export default AddPuppy;
